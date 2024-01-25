43971f6223e49283ffc31998581cf5fa860cbb7c
sara-essam-ata <samsarh194@gmail.com>
2024-01-25 21:46:51 +0200

    login ui

--- src/app/core/auth/components/login/login.component.ts
+++ src/app/core/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  hide: boolean = true;
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),
+    password: new FormControl(null, [Validators.required, Validators.pattern('^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),
+  })
+  constructor(
+    // private _AuthService:AuthService
+  ){}
+  // onSubmit(data:FormGroup){
+  //   this._AuthService.onLogIn(data.value).subscribe({
+  //     next:(res)=>{
 
+  //     }
+  //   })
+  // }
 }
