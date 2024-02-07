1b604bcd6736407def61dfd45153b432e56bdf08
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-02-07 20:05:31 +0200

    update

--- src/app/core/auth/components/register/register.component.ts
+++ src/app/core/auth/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Role } from '../../model/role.enum';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -16,6 +17,27 @@ export class RegisterComponent implements OnInit {
   hide:boolean=true;
   public RoleEnum = Role;
   public Role = Role.instructor;
+
+
+  registerForm = new FormGroup({
+    first_name: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('([a-zA-Z]){3,12}([0-9]{1,3})')
+    ]),
+    last_name: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('([a-zA-Z]){3,12}([0-9]{1,3})')
+    ]),
+    email: new FormControl(null,[Validators.required,Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')]),  
+
+    role: new FormControl(null, [
+      Validators.required
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')
+    ]),
+});
   
   
 
