6d461ce1624ae09b271989e2588c8b9373168651
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-02-09 19:12:07 +0200

    update

--- src/app/core/auth/components/change-password/change-password.component.ts
+++ src/app/core/auth/components/change-password/change-password.component.ts
@@ -20,23 +20,23 @@ export class ChangePasswordComponent {
   changePasswordForm = new FormGroup({
     password: new FormControl(null, [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),
     password_new: new FormControl(null, [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]),
-    confirm_password_new: new FormControl(null, [ Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')])
+    // confirm_password_new: new FormControl(null, [ Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')])
   },
-    { validators: this.passwordMatchValidator }
+    // { validators: this.passwordMatchValidator }
     )
 
-  passwordMatchValidator(control: any) {
-    let password_new = control.get('password_new');
-    let confirm_password_new = control.get('confirm_password_new')
-    if (password_new.value == confirm_password_new.value) {
-      return null;
-    } else {
-      control
-        .get('confirm_password_new')
-        ?.setErrors({ invalid: 'password and confirm password not match' });
-      return { invalid: 'password and confirm password not match' };
-    }
-  }
+  // passwordMatchValidator(control: any) {
+  //   let password_new = control.get('password_new');
+  //   let confirm_password_new = control.get('confirm_password_new')
+  //   if (password_new.value == confirm_password_new.value) {
+  //     return null;
+  //   } else {
+  //     control
+  //       .get('confirm_password_new')
+  //       ?.setErrors({ invalid: 'password and confirm password not match' });
+  //     return { invalid: 'password and confirm password not match' };
+  //   }
+  // }
 
   onSubmit(data: FormGroup){
     this._AuthService.onChangePassword(data.value).subscribe({
