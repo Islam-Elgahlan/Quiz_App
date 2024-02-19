fa82c7d06299b47d214bc2a27dc8e88040adcd9c
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-02-19 23:04:39 +0200

    update

--- src/app/core/pages/student/components/results-student/results-student.component.ts
+++ src/app/core/pages/student/components/results-student/results-student.component.ts
@@ -3,40 +3,55 @@ import { StudentService } from '../../service/student.service';
 import { ViewResultsComponent } from '../view-results/view-results.component';
 import { MatDialog } from '@angular/material/dialog';
 import { IAllResults } from '../../model/student';
+import { GroupsService } from '../../../instructor/modules/groupes/sevice/groups.service';
+import { IGroup } from '../../../instructor/modules/groupes/model/groups';
 
 @Component({
   selector: 'app-results-student',
   templateUrl: './results-student.component.html',
   styleUrls: ['./results-student.component.scss']
 })
-export class ResultsStudentComponent implements OnInit{
+export class ResultsStudentComponent implements OnInit {
 
-  resultsData:IAllResults[]|any;
-  quizData:IAllResults|any
+  resultsData: IAllResults | any;
+  quizData: IAllResults | any
+  groups: IGroup[] = [];
 
-  constructor(private _StudentService:StudentService, private dialog: MatDialog,){}
+
+  constructor(private _StudentService: StudentService, private dialog: MatDialog,
+    private _GroupsService: GroupsService,
+  ) { }
 
   ngOnInit(): void {
-    this.allResults()
+    this.allResults();
+    this.getAllGroups();
   }
 
-  allResults(){
+  allResults() {
     this._StudentService.getAllResults().subscribe({
-      next:(res)=>{
+      next: (res) => {
         console.log(res);
 
         this.resultsData = res;
-        console.log(this.resultsData);
-        
+
       }
     })
   }
 
-   // View
-   openViewDialog(enterAnimationDuration: string, exitAnimationDuration: string, data: any, view:boolean): void {
+  getAllGroups() {
+    this._GroupsService.onGetAllGroups().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.groups = res;
+      },
+    });
+  }
+
+  // View
+  openViewDialog(enterAnimationDuration: string, exitAnimationDuration: string, data: any, view: boolean): void {
     this.dialog.open(ViewResultsComponent, {
       width: '60%',
-      data: {data,view},
+      data: { data, view },
       enterAnimationDuration,
       exitAnimationDuration,
     });
